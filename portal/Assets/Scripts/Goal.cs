c27fa70989b3249d86c805dfa0bb79b14c5f6d6c
ctrl-Sebastian <74610997+ctrl-Sebastian@users.noreply.github.com>
2023-05-01 19:51:32 -0400

    Completed assignment 11, from portal

---
+++ portal/Assets/Scripts/Goal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public Text winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player"){
+            winText.text = "You Won!";
+            Debug.Log("You Win");
+        }
+    }
+}
